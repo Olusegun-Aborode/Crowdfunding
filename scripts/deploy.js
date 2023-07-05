afbab2b0471b273f72219d6c731828c96519ce20
Olusegun <110054818+Olusegun-Aborode@users.noreply.github.com>
2023-07-05 22:07:55 +0100

    Done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,27 @@
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
 
 async function main() {
-    const Crowdfunding = await ethers.getContractFactory("Crowdfunding");
-    const crowdfunding = await Crowdfunding.deploy();
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
 
-    await crowdfunding.deployed();
+  const lockedAmount = hre.ethers.parseEther("0.001");
 
-    console.log("Crowdfunding contract deployed to:", crowdfunding.address);
+  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Lock with ${ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  );
 }
 
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
